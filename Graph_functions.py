7e7c0631a916e6ebcf54924d195ac3453bf17323
Finley <Finleyrmarshall@gmail.com>
2023-12-08 14:52:05 +1300

    Edited the variance functions

--- Graph_functions.py
+++ Graph_functions.py
@@ -35,21 +35,26 @@ def graph_x1_and_p(satellite, prediction_data, estimate_data):
     predictions_x1 = []
     p_above = []
     p_below = []
+    offset = 0
 
-    for num in range(len(prediction_data)):
+    for num, time in enumerate(satellite.times):
         x1_prediction, x2_prediction, p_prediction = prediction_data[num]
         predictions_x1.append(x1_prediction)
-        p = p_prediction[0][0]
-        p_above.append(x1_prediction + two_standard_deviations(p))
-        p_below.append(x1_prediction - two_standard_deviations(p))
 
-    for num in range(len(estimate_data)):
-        x1_estimate, x2_estimate, p_estimate = estimate_data[num]
+        if time in satellite.measurements_times:
+            x1_estimate, x2_estimate, p_estimate = estimate_data[num - offset]
+        else:
+            offset += 1
+            x1_estimate, x2_estimate, p_estimate = x1_prediction, x2_prediction, p_prediction
+
+        p = p_estimate[0][0]
         estimate_x1.append(x1_estimate)
+        p_above.append(x1_estimate + two_standard_deviations(p))
+        p_below.append(x1_estimate - two_standard_deviations(p))
 
     plt.title('Dimension X1: Measurements, Prediction and P of X1 over time')
     plt.plot(satellite.times, satellite.x1, '.', label='X1')
-    plt.plot(satellite.times, predictions_x1, label='Prediction_x1')
+    plt.plot(satellite.times, estimate_x1, label='Estimate_x1')
     plt.plot(satellite.times, p_above, label='Two Standard deviations above')
     plt.plot(satellite.times, p_below, label='Two Standard deviations below')
 
@@ -83,16 +88,22 @@ def graph_x2_and_p(satellite, prediction_data, estimate_data):
     p_above = []
     p_below = []
 
-    for num in range(len(prediction_data)):
+    offset = 0
+
+    for num, time in enumerate(satellite.times):
         x1_prediction, x2_prediction, p_prediction = prediction_data[num]
-        p = p_prediction[1][1]
-        p_above.append(x2_prediction + two_standard_deviations(p))
-        p_below.append(x2_prediction - two_standard_deviations(p))
         predictions_x2.append(x2_prediction)
 
-    for num in range(len(estimate_data)):
-        x1_estimate, x2_estimate, p_estimate = estimate_data[num]
+        if time in satellite.measurements_times:
+            x1_estimate, x2_estimate, p_estimate = estimate_data[num - offset]
+        else:
+            offset += 1
+            x1_estimate, x2_estimate, p_estimate = x1_prediction, x2_prediction, p_prediction
+
+        p = p_estimate[1][1]
         estimate_x2.append(x2_estimate)
+        p_above.append(x2_estimate + two_standard_deviations(p))
+        p_below.append(x2_estimate - two_standard_deviations(p))
 
     plt.title('Dimension X2: Measurements, Prediction and P of X2 over time')
     plt.plot(satellite.times, satellite.x2, '.', label='X1')
@@ -108,7 +119,7 @@ def graph_error(satellite, prediction_data, estimate_data):
     measurements_error = []
     x1_estimate_error = []
     x2_estimate_error = []
-    h = satellite.times[1] - satellite.times[0]
+    h = satellite.h
 
     for num, time in enumerate(satellite.measurements_times):
         time = int(time // h)
